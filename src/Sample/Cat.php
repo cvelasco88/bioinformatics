fd7e102d497abfda22f6890cd83db7ce58e74f2f
Carlos Velasco <carlosvelasco69@hotmail.com>
2020-05-06 21:15:13 +0200

    started yii project

--- src/Sample/Cat.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace HelloWorld\Sample;
-
-class Cat {
-  public static function sayHelloWorld() {
-    return "I'm a cat\n";
-  }
-}
--- src/Sample/Dog.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace HelloWorld\Sample;
-
-class Dog {
-  public static function sayHelloWorld() {
-    return "I'm a dog\n";
-  }
-}
