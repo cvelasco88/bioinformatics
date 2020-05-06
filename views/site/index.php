445ee76d18f090d51b744954d71700ae5291e709
Carlos Velasco <carlosvelasco69@hotmail.com>
2020-05-06 21:53:31 +0200

    added chartjs library/widget php/js

--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use yiier\chartjs\ChartJs;
+
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
@@ -15,6 +17,26 @@ $this->title = 'My Yii Application';
     </div>
 
     <div class="body-content">
+	<?= ChartJs::widget([
+	    'type' => 'line',
+	    'options' => [
+		'height' => 200,
+		'width' => 600
+	    ],
+	    'data' => [
+		'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+		 'datasets' => [
+		     [
+		         'label'=> '# of Votes',
+		         'data' => [65, 59, 90, 81, 56, 55, 40]
+		     ],
+		     [
+		         'label'=> '# of Votes',
+		         'data' => [28, 48, 40, 19, 96, 27, 100]
+		     ]
+		 ]
+	    ]
+	]);?>
 
         <div class="row">
             <div class="col-lg-4">
